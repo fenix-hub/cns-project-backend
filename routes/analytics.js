a1e5db56a97e1d612c67719fbc483083c3f56b0d
fenix-hub <n.santilio97@gmail.com>
2023-10-29 21:16:20 +0100

    complete apis

--- routes/analytics.js
+++ routes/analytics.js
@@ -24,6 +24,7 @@ router.get('/streams', async (req, res) => {
     const aggregatedMetric = metrics[0];
     aggregatedMetric.viewList = views;
 
+
     // aggregatedMetric.totalViews = views.length;
     res.status(200).json(aggregatedMetric);
 });
@@ -34,52 +35,92 @@ router.get('/sessions', async (req, res) => {
     const userAgent = req.headers['user-agent'];
 
     // Extract session ID from cookies
-    const sessionId = req.query.id || req.session.id;
+    const streamId = req.query.id;
+    const sessionId = req.session.id;
+
+    const matchQ = {sessionId: sessionId};
+    if (streamId) {
+        matchQ.streamId = streamId;
+    }
 
     const metrics = await Metrics.aggregate([
-            { $match: { sessionId: sessionId } },
-            { $sort: { timestamp: -1 } },
-            {
-                $group: {
-                    _id: {
-                        streamId: '$streamId',
-                        mediaLevel: '$mediaLevel.level',
-                    },
-                    totalStreamedTime: { $sum: '$streamedTime' },
-                    totalStreamedBytes: { $sum: '$downloadedBytes' },
-                    bufferingEvents: { $sum: { $size: '$bufferings' } },
-                    bufferingTime: { $sum: { $sum: '$bufferings.duration' } },
-                    screenSizes: { $addToSet: '$screenSize' },
-                    downloadRate: { $avg: '$downloadRate' },
-                    bandwidth: { $avg: '$bandwidth' },
+        {$match: matchQ},
+        {$sort: {timestamp: -1}},
+        {
+            $group: {
+                _id: {
+                    streamId: '$streamId',
+                    mediaLevel: '$mediaLevel.level',
                 },
+                sessionId: {$first: '$sessionId'},
+                userAgent: {$first: '$userAgent'},
+                clientIp: {$first: '$clientIp'},
+                totalStreamedTime: {$sum: '$streamedTime'},
+                totalStreamedBytes: {$sum: '$downloadedBytes'},
+                bufferingEvents: {$sum: {$size: '$bufferings'}},
+                bufferingTime: {$sum: {$sum: '$bufferings.duration'}},
+                downloadRate: {$avg: '$downloadRate'},
+                bandwidth: {$avg: '$bandwidth'},
             },
-            {
-                $group: {
-                    _id: '$_id.streamId', // Group by streamId
-                    mediaLevels: {
-                        $push: {
-                            level: '$_id.mediaLevel',
-                            totalStreamedTime: '$totalStreamedTime',
-                        },
+        },
+        {
+            $group: {
+                _id: '$_id.streamId', // Group by streamId
+                mediaLevels: {
+                    $push: {
+                        level: '$_id.mediaLevel',
+                        totalStreamedTime: '$totalStreamedTime',
                     },
-                    totalStreamedTime: { $sum: '$totalStreamedTime' },
-                    totalStreamedBytes: { $sum: '$totalStreamedBytes' },
-                    bufferingEvents: { $sum: '$bufferingEvents' },
-                    bufferingTime: { $sum: '$bufferingTime' },
-                    screenSizes: { $addToSet: '$screenSizes' },
-                    downloadRate: { $avg: '$downloadRate' },
-                    bandwidth: { $avg: '$bandwidth' },
                 },
-
+                sessionId: {$first: '$sessionId'},
+                userAgent: {$first: '$userAgent'},
+                clientIp: {$first: '$clientIp'},
+                totalStreamedTime: {$sum: '$totalStreamedTime'},
+                totalStreamedBytes: {$sum: '$totalStreamedBytes'},
+                bufferingEvents: {$sum: '$bufferingEvents'},
+                bufferingTime: {$sum: '$bufferingTime'},
+                downloadRate: {$avg: '$downloadRate'},
+                bandwidth: {$avg: '$bandwidth'},
             },
+        },
     ]);
 
+    const screenSizesMetrics = await Metrics.aggregate([
+        {$match: {sessionId: sessionId}},
+        {$sort: {timestamp: -1}},
+        {
+            $group: {
+                _id: {
+                    streamId: '$streamId',
+                    screenSize: '$screenSize',
+                },
+                totalStreamedTime: {$sum: '$streamedTime'},
+            },
+        },
+        {
+            $group: {
+                _id: '$_id.streamId', // Group by streamId
+                screenSizes: {
+                    $push: {
+                        size: '$_id.screenSize',
+                        totalStreamedTime: '$totalStreamedTime',
+                    },
+                },
+            },
+        },
+    ]);
+
+
+    for (i = 0; i < metrics.length; i++) {
+        metrics[i]['screenSizes'] = screenSizesMetrics[i]['screenSizes'];
+    }
+
     for (const metric of metrics) {
-        metric.screenSizes = metric.screenSizes.flat();
-        let total
         for (const mediaLevel of metric.mediaLevels) {
             mediaLevel['%totalStreamedTime'] = mediaLevel.totalStreamedTime / metric.totalStreamedTime * 100;
+        }
+        for (const screenSize of metric.screenSizes) {
+            screenSize['%totalStreamedTime'] = screenSize.totalStreamedTime / metric.totalStreamedTime * 100;
         }
     }
     res.status(200).json(metrics);
